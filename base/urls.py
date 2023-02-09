b7e8d9b383e3d34a555cf48c655826fbaf6767b6
Igho <ambassadorj.boy@gmail.com>
2023-02-09 21:57:11 +0100

    09-02-22 commit

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('product/', views.product, name='product'),
+    path('contact/', views.contact, name='contact'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
 ]
--- base/views.py
+++ base/views.py
@@ -5,6 +5,18 @@ def index(request):
     context = {}
     return render(request, 'base/index.html', context)
 
+def about(request):
+    context = {}
+    return render(request, 'base/about.html', context)
+
+def product(request):
+    context = {}
+    return render(request, 'base/product.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'base/contact.html', context)
+
 def cart(request):
     context = {}
     return render(request, 'base/cart.html', context)
