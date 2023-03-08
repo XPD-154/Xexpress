9496d8e1e00f5c01d80356aaaf38e0e56ef1cdef
Igho <ambassadorj.boy@gmail.com>
2023-03-08 14:06:53 +0100

    08-03-23 commit

--- base/views.py
+++ base/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import *               #import all models into view
 
 # Create your views here.
 def index(request):
-    context = {}
+    products = Product.objects.all().order_by('id')[:3]
+    context = {'products':products}
     return render(request, 'base/index.html', context)
 
 def about(request):
@@ -10,7 +12,8 @@ def about(request):
     return render(request, 'base/about.html', context)
 
 def product(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products':products}
     return render(request, 'base/product.html', context)
 
 def contact(request):
@@ -18,7 +21,18 @@ def contact(request):
     return render(request, 'base/contact.html', context)
 
 def cart(request):
-    context = {}
+    #check if user is authenticated
+    if request.user.is_authenticated:
+        #get customer
+        customer = request.user.customer
+        #check for customer's order create one or check for existing
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        #get all orderitems that have order on top as parents
+        items = order.orderitem_set.all()
+    else:
+        items = []
+
+    context = {'items':items}
     return render(request, 'base/cart.html', context)
 
 def checkout(request):
