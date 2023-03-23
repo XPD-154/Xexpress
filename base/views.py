abd460427c9b59175afd15507344d39fc0fe67da
Igho-Iggue Iruesiri <ambassadorj.boy@gmail.com>
2023-03-23 16:54:10 +0100

    3-23-2023 commit

--- base/views.py
+++ base/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
-from .models import *               #import all models into view
+from .models import *                   #import all models into view
+from django.http import JsonResponse
+import requests
+import json
 
-# Create your views here.
+#Create your views here.
+
+'''check and update cart item total based on items added'''
+
+def cartItems(request, number):
+    request.session['cartItems'] = number
+    return request.session['cartItems']
+
+def cartItemsCheck(request):
+    if 'cartItems' in request.session:
+        cartData = request.session['cartItems']
+    else:
+        cartData = cartItems(request, 0)
+
+    return cartData
+
+'''end of check and update cart item total based on items added'''
+
 def index(request):
     products = Product.objects.all().order_by('id')[:3]
-    context = {'products':products}
+
+    #get cart item total
+    cartData = cartItemsCheck(request)
+
+    context = {'products':products, 'cartItems':cartData}
     return render(request, 'base/index.html', context)
 
 def about(request):
@@ -13,7 +37,29 @@ def about(request):
 
 def product(request):
     products = Product.objects.all()
-    context = {'products':products}
+
+    #check if user is authenticated
+    if request.user.is_authenticated:
+        #get customer
+        customer = request.user.customer
+        #check for customer's order create one or check for existing
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        #get all orderitems that have order on top as parents
+        items = order.orderitem_set.all()
+
+        #get cart item total
+        cartItemsNumber = order.get_cart_items
+        cartData = cartItems(request, cartItemsNumber)
+
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+        #get cart item total
+        cartItemsNumber = order['get_cart_items']
+        cartData = cartItems(request, cartItemsNumber)
+
+    context = {'products':products, 'cartItems':cartData}
     return render(request, 'base/product.html', context)
 
 def contact(request):
@@ -29,11 +75,20 @@ def cart(request):
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         #get all orderitems that have order on top as parents
         items = order.orderitem_set.all()
+
+        #get cart item total
+        cartItemsNumber = order.get_cart_items
+        cartData = cartItems(request, cartItemsNumber)
+
     else:
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
 
-    context = {'items':items, 'order':order}
+        #get cart item total
+        cartItemsNumber = order['get_cart_items']
+        cartData = cartItems(request, cartItemsNumber)
+
+    context = {'items':items, 'order':order, 'cartItems':cartData}
     return render(request, 'base/cart.html', context)
 
 def checkout(request):
@@ -49,5 +104,39 @@ def checkout(request):
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
 
-    context = {'items':items, 'order':order}
+    #get cart item total
+    cartData = cartItemsCheck(request)
+
+    context = {'items':items, 'order':order, 'cartItems':cartData}
     return render(request, 'base/checkout.html', context)
+
+def updateItem(request):
+
+    #load items from javascript API and make items available
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:', action)
+    print('Product:', productId)
+
+    #get signed in customer, the product, order and order item
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    #add or remove from orderitem quantity
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    #save after changes
+    orderItem.save()
+
+    #if orderitem quantity is zero, delete
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('item was added', safe=False)
+
--- static/js/cart.js
+++ static/js/cart.js
@@ -9,10 +9,39 @@ for(i=0; i<updateBtns.length; i++){
         console.log('productId:', productId, 'action:', action)
 
         console.log('USER:', user)
-        if(user=='AnonymousUser'){
-            console.log('user is not authenticated')
+        if(user==='AnonymousUser'){
+            console.log('Not Logged in')
         }else{
-            console.log('User is authenticated. sending data...')
+            /*console.log('User is authenticated. sending data...')*/
+            updateUserOrder(productId, action)
         }
+    })
+}
+
+function updateUserOrder(productId, action){
+    console.log("User is already authenticated, sending data..")
+
+    /*url to send the data of productId and action*/
+    var url = '/update_item/'
+
+    /*api to use to send data*/
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'Accept': 'application/json',
+            'X-CSRFToken':csrftoken,
+        },
+        body: JSON.stringify({'productId': productId, 'action': action})
+    })
+
+    /*after the fetch is complete, return data*/
+    .then((response) => {
+        return response.json()
+    })
+
+    .then((data) => {
+        //console.log('datajs:',data)
+        location.reload()
     })
 }
